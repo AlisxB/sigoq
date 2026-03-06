a9aa99023588e5930ba9802e5cb8bf52850deb7b
alisxb <alisxb@example.com>
2026-03-06 00:30:02 -0300

    [config] Modifica permissão padrão do DRF para AllowAny em dev

--- sigoq/settings.py
+++ sigoq/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # rest-framework configuration
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
